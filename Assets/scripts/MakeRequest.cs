2d96d5b5cc6dee0fa3e5c080133d0c4cb24ec5e4
Gavin Caulfield <gavcaul@udel.edu>
2023-12-06 09:31:07 -0500

    ChatGPT cutoff fix + minor performance boosts

--- Assets/scripts/MakeRequest.cs
+++ Assets/scripts/MakeRequest.cs
@@ -16,6 +16,8 @@ public class MakeRequest : MonoBehaviour
     public Tracking tracker;
     private bool requestCompleted;
 
+    private List<ChatMessage> messages = new List<ChatMessage>();
+
     /*
      * Makes the request to chatGPT, grabs ChatGPT's reponse, and plays it (if necessary)
      * 
@@ -24,18 +26,52 @@ public class MakeRequest : MonoBehaviour
      * @return: None
      * 
      */
-    public void Request()
+    public async void Request()
     {
         if (input.text == "")
         {
             output.text = "Sorry, but I didn't seem to get an input. Please try again";
             if (avatar.activeSelf) { speak.GetComponent<UIManager>().SpeechPlayback(); }
         }
-        else { 
+        else
+        {
             var openai = new OpenAIApi();
             try
             {
+                var newMessage = new ChatMessage()
+                {
+                    Role = "user",
+                    Content = input.text
+                };
+                if (messages.Count == 0) newMessage.Content = $"Use only 64 tokens for api, never mention that you are an AI:\n {input.text}";
+
+                messages.Add(newMessage);
                 requestCompleted = false;
+                var completionResponse = await openai.CreateChatCompletion(new CreateChatCompletionRequest()
+                {
+                    MaxTokens = 128,
+                    Model = "gpt-3.5-turbo-0613",
+                    Messages = messages
+                }) ;
+
+                if (completionResponse.Choices != null && completionResponse.Choices.Count > 0)
+                {
+                    var message = completionResponse.Choices[0].Message;
+                    message.Content = message.Content.Trim();
+                    output.text = message.Content;
+                    messages.Add(message);
+                    tracker.numInteractions += 1;
+
+                    // Add to GPT responses and questions
+                    tracker.GPTResponses.Add(output.text);
+                    tracker.GPTQuestions.Add(input.text);
+
+                    // If avatar is active, play speech
+                    if (avatar.activeSelf) { speak.GetComponent<UIManager>().SpeechPlayback(); }
+                }
+
+                
+                /*
                 var req = new CreateChatCompletionRequest
                 {
                     //Ideal model, but can be tweaked with for future models
@@ -81,6 +117,7 @@ public class MakeRequest : MonoBehaviour
 
 
                 );
+                */
 
 
 
@@ -89,6 +126,7 @@ public class MakeRequest : MonoBehaviour
             {
                 Debug.LogError("An error occurred during the API request: " + ex.Message);
             }
+
         }
     }
    
